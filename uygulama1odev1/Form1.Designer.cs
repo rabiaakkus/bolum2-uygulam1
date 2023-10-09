b8aeed4a7f044e0b3a0949e0a9f225e2835bd72f
rabiaakkus <145451092+rabiaakkus@users.noreply.github.com>
2023-10-09 10:18:01 +0300

    Add files via upload

---
+++ uygulama1odev1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace uygulama1odev1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.btnKarşılaştır = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Maroon;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(25, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "bir sayıyı giriniz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(25, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "iki sayıyı giriniz";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(112, 31);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi1.TabIndex = 2;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(105, 83);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi2.TabIndex = 3;
+            // 
+            // btnKarşılaştır
+            // 
+            this.btnKarşılaştır.Location = new System.Drawing.Point(66, 128);
+            this.btnKarşılaştır.Name = "btnKarşılaştır";
+            this.btnKarşılaştır.Size = new System.Drawing.Size(75, 23);
+            this.btnKarşılaştır.TabIndex = 4;
+            this.btnKarşılaştır.Text = "karşılaştır";
+            this.btnKarşılaştır.UseVisualStyleBackColor = true;
+            this.btnKarşılaştır.Click += new System.EventHandler(this.btnKarşılaştır_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(224, 181);
+            this.Controls.Add(this.btnKarşılaştır);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Button btnKarşılaştır;
+    }
+}
+
---
+++ uygulama1odev1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama1odev1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKarşılaştır_Click(object sender, EventArgs e)
+        {
+            byte sayı1, sayı2;
+            sayı1 = Convert.ToByte(txtSayi1.Text);
+            sayı2 = Convert.ToByte(txtSayi2.Text);
+            if (sayı1 > sayı2)
+            {
+                MessageBox.Show("1.sayı 2.sayıdan büyüktür");
+
+            }
+            if (sayı1 == sayı2)
+            {
+                MessageBox.Show("sayılar birbirine eşitir");
+
+            }
+            if (sayı1 < sayı2)
+            {
+                MessageBox.Show("2.sayı 1.sayıdan buyuktur");
+            }
+        }
+    }
+}
